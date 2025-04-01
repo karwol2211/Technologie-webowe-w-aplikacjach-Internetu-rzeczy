8207d8e8f84114e89ac27005250773ffb6f57d7f
karwol2211 <134288771+karwol2211@users.noreply.github.com>
2025-04-01 17:53:06 +0200

    Update config.ts

--- IoT-dashboard/api/lib/config.ts
+++ IoT-dashboard/api/lib/config.ts
@@ -1,5 +1,5 @@
 export const config = {
     port: process.env.PORT || 3100,
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://KarolWolosowski:6FxiJsDhvD6v9REg@cluster0.wzzkpvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    databaseUrl: process.env.MONGODB_URI || '',
 
  };
